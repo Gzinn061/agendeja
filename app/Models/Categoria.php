5803434c1a0f4a568a0f76ac8a0e84550fac7b24
Gzinn061 <martinsnoguilherme@gmail.com>
2025-04-30 16:04:14 -0300

    Dia 02: Models & relationship Service - Categories working

--- app/Models/Categoria.php
+++ app/Models/Categoria.php
@@ -2,9 +2,17 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Servico;
 
 class Categoria extends Model
 {
-    //
+    use HasFactory;
+
+    // Relação com os serviços
+    public function servicos()
+    {
+        return $this->hasMany(Servico::class);
+    }
 }
--- app/Models/Servico.php
+++ app/Models/Servico.php
@@ -2,9 +2,16 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Categoria;
 
 class Servico extends Model
 {
-    //
+    use HasFactory;
+
+    public function categoria()
+    {
+        return $this->belongsTo(Categoria::class);
+    }
 }
